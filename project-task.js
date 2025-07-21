c62772523b5f0cd223534b7f0ab78c19a4a7f6f3
aidanbeck <aidanmbeck@outlook.com>
2025-07-20 19:38:59 -0500

    completed assignment

--- project-task.js
+++ project-task.js
@@ -47,24 +47,24 @@ but it contains multiple syntax errors that prevent it from being parsed.
 const invalidBookingJSON = `
 {
   "hotelName": "Grand City Hotel",
-  "checkInDate": "2024-05-15"
+  "checkInDate": "2024-05-15", ${/*comma after value*/''}
   "checkOutDate": "2024-05-20",
   "guests": [
     {
-      name: "Alice Johnson",
+      "name": "Alice Johnson", ${/*keys need quotation marks*/''}
       "age": 30,
       "email": "alice.johnson@example.com"
     },
     {
       "name": "Bob Smith",
-      "age": undefined,
+      "age": null, ${/*undefined not an allowed type, use null*/''}
       "email": "bob.smith@example"
     }
   ],
   "roomDetails": {
     "type": "Suite",
     "pricePerNight": 200,
-    "amenities": ["WiFi", "Breakfast", "Parking",]
+    "amenities": ["WiFi", "Breakfast", "Parking"] ${/*final comma deleted*/''}
   }
 }
 `;
@@ -91,11 +91,19 @@ const invalidBookingJSON = `
 💬 Reflect and answer the following:
 
 1️⃣ What tools or techniques did you use to identify the errors?
+- I used my notes of what is allowed in JSON, and the linter to catch everything else.
 
 2️⃣ How did you confirm that your corrected JSON file was valid?
+- I pasted the json into the linter and it verified it as valid.
 
 3️⃣ Which errors were the most difficult to spot? Why?
+- I did not catch the undefined error until the linter, as I forgot that undefined isn't a valid value in JSON
 
 4️⃣ What strategies can help you avoid these kinds of errors in the future?
    (e.g., syntax highlighting, linters, writing JSON by example)
+- I like to write JSON in VSCode where the formatting indicators will automatically underline something if it isn't quite right.
+- This tips me off early to any problems, so I can fix them before they become a needle in a haystack.
+- It's like someone pointing out the needle when it's still on top, before it gets covered in more hay.
+
+Bonus: I am not sure if this was intended, but I'm a little proud of my solution for adding comments within a string literal.
 */
